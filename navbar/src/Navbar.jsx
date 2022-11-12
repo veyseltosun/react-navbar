4bf250a6f91b18016d3f8f96caa107c57e4e1ddd
veyseltosun <postalarim1923@gmail.com>
2022-11-12 18:29:27 +0300

    useRef using.

--- navbar/src/Navbar.jsx
+++ navbar/src/Navbar.jsx
@@ -6,6 +6,18 @@ import logo from "./logo.svg"
 
 function Navbar() {
     const[showLinks, setShowLinks] = useState(false)
+    const linksContainerRef = useRef(null);
+    const linksRef = useRef(null);
+
+    useEffect( () => {
+       const linksHeight = linksRef.current.getBoundingClientRect().height 
+       if(showLinks){
+        linksContainerRef.current.style.height = `${linksHeight}px`
+       }else{
+        linksContainerRef.current.style.height = "0px"
+       }
+
+    },[showLinks])
   return (
     <nav>
         <div className='nav-center'>
@@ -17,8 +29,8 @@ function Navbar() {
             </div>
             
 
-            <div className="links-container">
-                <ul className='links'>
+            <div className="links-container" ref={linksContainerRef}>
+                <ul className='links' ref={linksRef}>
                    {links.map((link)=>{
                     const{id, text, url} = link;
                     return(
