8e37e70a0d9bded40dbc4245c2ff3fd5a08db8c7
veyseltosun <postalarim1923@gmail.com>
2022-11-12 17:33:55 +0300

    data links property and maping them.

--- navbar/src/Navbar.jsx
+++ navbar/src/Navbar.jsx
@@ -16,18 +16,14 @@ function Navbar() {
             </div>
             <div className='links-container show-container'>
                 <ul className='links'>
-                    <li>
-                        <a href='#'>home</a>
-                    </li>
-                    <li>
-                        <a href='#'>about</a>
-                    </li>
-                    <li>
-                        <a href='#'>contact</a>
-                    </li>
-                    <li>
-                        <a href='#'>products</a>
-                    </li>
+                   {links.map((link)=>{
+                    const{id, text, url} = link;
+                    return(
+                        <li key={id}>
+                            <a href={url}>{text}</a>
+                        </li>
+                    )
+                   })}
                 </ul>
             </div>
             <ul className='social-icons'>
